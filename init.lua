6472151441276c6144fae4f4c95a0d91a5288023
Per Magnus <magnus@pmagnus.dk>
2023-03-03 12:55:06 +0000

    Add rust/go LSP

--- init.lua
+++ init.lua
@@ -407,10 +407,10 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
+  -- -- clangd = {},
+  gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
 
   lua_ls = {
